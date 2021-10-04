5561cc416d1f78ce2d80b733cdcc63e0e54b4eb6
chinenye ozoagudike <chinenyeozoagudike@gmail.com>
2021-10-04 09:35:57 +0100

    initial commit

---
+++ EmployeesApi/Controllers/WeatherForecastController.cs
@@ -0,0 +1,46 @@
+﻿using Contracts;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace EmployeesApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private ILoggerManager _logger;
+        public WeatherForecastController(ILoggerManager logger)
+        {
+           _logger = logger;
+        }
+
+        public IEnumerable<string> Get()
+        {
+            _logger.LogInfo("Here is info message from our values controller.");
+            _logger.LogDebug("Here is debug message from our values controller.");
+           _logger.LogWarn("Here is warn message from our values controller.");
+           _logger.LogError("Here is an error message from our values controller.");
+            return new string[] { "value1", "value2" };
+        }
+
+        //private readonly IRepositoryManager _repository;
+        //public WeatherForecastController(IRepositoryManager repository)
+        //{
+        //    _repository = repository;
+        //}
+        //[HttpGet]
+        //public ActionResult<IEnumerable<string>> Get()
+        //{
+        //    _repository.Company.AnyMethodFromCompanyRepository();
+        //    _repository.Employee.AnyMethodFromEmployeeRepository();
+        //    return new string[] { "value1", "value2" };
+
+        //}
+
+
+}   }
+
+        
+
+    
+
---
+++ EmployeesApi/Extensions/ServiceExtensions.cs
@@ -0,0 +1,46 @@
+﻿using Contracts;
+using Entities.Models;
+using LoggerService;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EmployeesApi.Extensions
+{
+    public static class ServiceExtensions
+    {
+        public static void ConfigureCors(this IServiceCollection services) =>
+             services.AddCors(options =>
+             {
+                 options.AddPolicy("CorsPolicy", builder =>
+                 builder.AllowAnyOrigin()
+                 .AllowAnyMethod()
+                 .AllowAnyHeader());
+             });
+
+        public static void ConfigureIISIntegration(this IServiceCollection services) =>
+           services.Configure<IISOptions>(options =>
+           {
+           });
+
+        public static void ConfigureLoggerService(this IServiceCollection services) =>
+            services.AddScoped<ILoggerManager, LoggerManager>();
+
+        public static void ConfigureSqlContext(this IServiceCollection services,
+            IConfiguration configuration) =>
+             services.AddDbContext<RepositoryContext>(opts =>
+             opts.UseSqlServer(configuration.GetConnectionString("sqlConnection"), b => b.MigrationsAssembly("EmployeesApi")));
+
+
+        public static void ConfigureRepositoryManager(this IServiceCollection services) =>
+            services.AddScoped<IRepositoryManager, RepositoryManager>();
+
+
+    }
+}
